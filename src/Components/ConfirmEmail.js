3485e87d11c032cfdf988f96c4e9f7eedf45c07d
yazharasu <yazh.bis@gmail.com>
2022-05-29 18:31:13 +0530

    c

--- src/Components/ConfirmEmail.js
+++ src/Components/ConfirmEmail.js
@@ -8,7 +8,7 @@ export default function ConfirmEmail( { companyData, setCompanyData, setSetupApp
 
   const clickHandler = async (e) => {
     e.preventDefault();
-    const res = await axios.get( '/api/auth/checkEmail' , { "email" : email.current.value} )
+    const res = await axios.get( '/auth/checkEmail' , { "email" : email.current.value} )
     console.log( res );
 
     if( res === "Not available") {
@@ -26,28 +26,27 @@ export default function ConfirmEmail( { companyData, setCompanyData, setSetupApp
 
   return (
     <div class="d-flex-column align-items-center">
-        <p class='h3 fw-bold pe-3' style={{"width": "390px"}} >Make Your Life Easy with Intranet!</p>
-        <p class="small text-secondary">To make a workplace from scratch, please confirm your eamil</p>
-        
-        <form class='mt-4' onSubmit={ clickHandler }>
-            <div class="mb-3">
-              <label for="inputEmail1" class="form-label small text-secondary p-0 m-0" style={{"color": "grey"}} >Email address</label>
-              <input type="email" class="form-control fs-6 ps-1 text-secondary small" ref={email} id="inputEmail1" style={{"width": "390px"}} aria-describedby="emailHelp" placeholder='name@email.com' />
-            </div>
-            <button type="submit" class="btn btn-success px-4 mt-3" ><small>Confirm Email</small></button>
-        </form>
-
-        { ( emailCheck==="Already available") &&  
-          <div class="d-flex flex-row" >
-            <p class="small text-failure">Entered email is already in use.</p>
+      <p class='h3 fw-bold pe-3' style={{"width": "390px"}} >Make Your Life Easy with Intranet!</p>
+      <p class="small text-secondary">To make a workplace from scratch, please confirm your eamil</p>
+      
+      <form class='mt-4' onSubmit={ clickHandler }>
+          <div class="mb-3">
+            <label for="inputEmail1" class="form-label small text-secondary p-0 m-0" style={{"color": "grey"}} >Email address</label>
+            <input type="email" class="form-control fs-6 ps-1 text-secondary small" ref={email} id="inputEmail1" style={{"width": "390px"}} aria-describedby="emailHelp" placeholder='name@email.com' />
           </div>
-        }
-        { ( emailCheck==="Something went wrong") &&  
-          <div class="d-flex flex-row" >
-            <p class="small text-failure">Something went wrong. Try again later.</p>
-          </div>
-        }
+          <button type="submit" class="btn btn-success px-4 mt-3" ><small>Confirm Email</small></button>
+      </form>
 
+      { ( emailCheck==="Already available") &&  
+        <div class="d-flex flex-row" >
+          <p class="small text-failure">Entered email is already in use.</p>
+        </div>
+      }
+      { ( emailCheck==="Something went wrong") &&  
+        <div class="d-flex flex-row" >
+          <p class="small text-failure">Something went wrong. Try again later.</p>
+        </div>
+      }
     </div>
   )
 }
